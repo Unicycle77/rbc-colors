bdf75c15d531c37b65c001b0df11580f0b37bed3
Jeffrey Martin <unicycle77@gmail.com>
2021-08-09 14:58:50 -0700

    Create palettes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import Palette from "./Palette";
+import seedPalettes from "./seedPalettes";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Palette {...seedPalettes[4]}/>
       </div>
     );
   }
---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <div className="Palette">
+          {/* Navbar goes here */}
+          <div className="Palette-colors">
+            {/* bunch of color boxes */}
+          </div>
+          {/* Footer goes here*/}
+        </div>
+      </div>
+    )
+  }
+}
