c669b4206f5844f78828d1188ecfdc7b715aa38f
Jeffrey Martin <unicycle77@gmail.com>
2024-05-10 16:37:49 -0700

    Style color slider

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import ColorBox from "../ColorBox/ColorBox";
+import 'rc-slider/assets/index.css'
 import "./Palette.css";
 import Slider from "rc-slider";
-import 'rc-slider/assets/index.css'
 
 class Palette extends Component {
   constructor(props) {
@@ -27,7 +27,9 @@ class Palette extends Component {
     return (
       <div>
         <div className="Palette">
-        <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+          <div className="slider">
+            <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+          </div>
           {/* Navbar goes here */}
           <div className="Palette-colors">
             {/* bunch of color boxes */}
