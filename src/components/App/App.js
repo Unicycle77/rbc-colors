de92b9b93b09a6ad85a5d2db3967ac85c0b05333
Jeffrey Martin <unicycle77@gmail.com>
2024-05-10 16:11:55 -0700

    Add unstyled color slider

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Palette from "../Palette/Palette";
 import seedColors from "./seedColors";
-import { generatePalette } from "../../colorHelper";
+import { generatePalette } from "../../colorHelpers";
 
 class App extends Component {
   render() {
     console.log(generatePalette(seedColors[4]));
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import ColorBox from "../ColorBox/ColorBox";
 import "./Palette.css";
+import Slider from "rc-slider";
+import 'rc-slider/assets/index.css'
 
-export default class Palette extends Component {
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      level: 500
+    }
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(level) {
+    this.setState({level})
+  }
+
   render() {
-    const colorBoxes = this.props.colors.map(color => (
-      <ColorBox key={color.color} background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    
+    const colorBoxes = colors[level].map(color => (
+      <ColorBox key={color.id} background={color.hex} name={color.name} />
     ))
     return (
       <div>
         <div className="Palette">
+        <Slider defaultValue={this.state.level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
           {/* Navbar goes here */}
           <div className="Palette-colors">
             {/* bunch of color boxes */}
@@ -21,3 +39,5 @@ export default class Palette extends Component {
     );
   }
 }
+
+export default Palette;
